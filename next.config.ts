0ba4f1567722b115c3eb04c01865e72e47420aba
zapinhor <112128255+zapinhor@users.noreply.github.com>
2026-08-23 18:28:45 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio",
+  basePath: "/Portifolio",
+  assetPrefix: "/Portifolio",
   trailingSlash: true,
   typescript: { ignoreBuildErrors: true },
   experimental: { workerThreads: true, cpus: 1 },
